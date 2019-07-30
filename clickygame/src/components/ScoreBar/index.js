f5a666d07c989edc07d1461e1cff0deee3535f31
Brittany McNulty <Brittany.m.mcnulty@gmail.com>
2019-07-29 22:22:06 -0500

    functiionality working

---
+++ clickygame/src/components/ScoreBar/index.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import "./ScoreBar.css";
+
+const ScoreBar = props => (
+    <div className="score-bar text-center">
+        <h3>Score: {props.score}</h3>
+    </div>
+);
+
+export default ScoreBar;
