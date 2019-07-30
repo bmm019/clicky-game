f50acce5ddeb4a03095277fbf4f29e20fc5e6ca6
Brittany McNulty <Brittany.m.mcnulty@gmail.com>
2019-07-30 14:42:49 -0500

    Updates

---
+++ precache-manifest.caf1dc19c852b2a9381e52e1ea08cb90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7562eb48bae975c6ff0c843deb0323ab",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d1302f5bd1a61b6fa117",
+    "url": "/clicky-game/static/css/main.5e28729b.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "d1302f5bd1a61b6fa117",
+    "url": "/clicky-game/static/js/main.fe437661.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
