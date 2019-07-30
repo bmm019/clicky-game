ff0398719c9165513fe90d663c0e42881b640b32
Brittany McNulty <Brittany.m.mcnulty@gmail.com>
2019-07-30 14:40:08 -0500

    moving app up a folder

---
+++ build/precache-manifest.e05536a99c6b8ae3a6b46edc834bf83b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66ef02de55c7dd31037a1fd71ea0ff0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b427ed299ff303303b02",
+    "url": "/clicky-game/static/css/main.5e28729b.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b427ed299ff303303b02",
+    "url": "/clicky-game/static/js/main.d4426026.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
